be7dedbfcca6fb6003d561464e3de747f17d16ff
Botista4 <135262016+Botista4@users.noreply.github.com>
2023-06-01 20:50:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,98 @@
+import telebot
+from instagrapi import Client
+import random
+import os
+import shutil
+from datetime import datetime
+from server import server
+
+
+API_KEY = '6023417756:AAHqWH9J1dBb_A-JoM33AVUisW1jw9aBhcA'
+CHATID = '5966905118'
+bot = telebot.TeleBot(API_KEY)
+cl = Client()
+server()
+
+def tbot():
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        bot.send_chat_action(message.chat.id, action='typing')
+        smsg = "Botista is up!\nSend me an Instagram link (Photo, Reel, IGTV, Album) and I will download it for you <3"
+        bot.reply_to(message, smsg)
+
+    @bot.message_handler(func=lambda m: True)
+    def get_media(message):
+        m = message.text
+        print(m)
+        try:
+            x = cl.media_pk_from_url(m)
+            info = cl.media_info(x).product_type
+            media = cl.media_info(x).media_type
+        except:
+            info = None
+            media = None
+            bot.reply_to(message, "Unvalid, please send an Instagram link.")
+            
+        userId = message.chat.id
+        nameUser = str(message.chat.first_name) + ' ' + str(message.chat.last_name)
+        username = message.chat.username
+        text = message.text
+        date = datetime.now()
+        data = f'User id: {userId}\nUsermae: @{username}\nName: {nameUser}\nText: {text}\nDate: {date}'
+        bot.send_message(chat_id=CHATID, text=data)
+
+        if info == "clips":
+            delete = bot.reply_to(message, "Downloading reel...")
+            clip_url = cl.media_info(x).video_url
+            n = str(random.randint(0,19))
+            reelname = "reel" + n
+            reel = cl.clip_download_by_url(clip_url,reelname)
+            bot.send_chat_action(message.chat.id, action='upload_video')
+            bot.delete_message(message.chat.id, delete.message_id)
+            bot.send_video(chat_id=message.chat.id, video=open(reel, 'rb'), timeout=200)
+
+        if info == "igtv":
+            delete = bot.reply_to(message, "Downloading igtv...")
+            n = str(random.randint(0,19))
+            tvname = "tv" + n
+            tvurl = cl.media_info(x).video_url
+            tv = cl.igtv_download(tvurl, tvname)
+            bot.send_chat_action(message.chat.id, action='upload_video')
+            bot.delete_message(message.chat.id, delete.message_id)
+            bot.send_video(chat_id=message.chat.id, video=open(tv, 'rb'), timeout=200)
+
+        if media == 1:
+            delete = bot.reply_to(message, "Downloading Photo...")
+            n = str(random.randint(0,19))
+            picname = "pic" + n
+            if os.path.exists(picname):
+                shutil.rmtree(picname)
+            os.mkdir(picname)
+            photo = cl.photo_download(x, picname)
+            bot.send_chat_action(message.chat.id, action='upload_photo')
+            bot.delete_message(message.chat.id, delete.message_id)
+            bot.send_photo(chat_id=message.chat.id, photo=open(photo, 'rb'), timeout=200)
+
+        if media == 8:
+            delete = bot.reply_to(message, "Downloading Album...")
+            n = str(random.randint(0,19))
+            pname = "p" + n
+            if os.path.exists(pname):
+                shutil.rmtree(pname)
+            os.mkdir(pname)
+            album = cl.album_download(x, folder=pname)
+            bot.delete_message(message.chat.id, delete.message_id)
+            for item in album:
+                item = str(item)
+                if ".heic" in item or ".webp" in item or ".jpg" in item or ".jpeg" in item or ".png" in item:
+                    bot.send_chat_action(message.chat.id, action='upload_photo',)
+                    bot.send_photo(chat_id=message.chat.id, photo=open(item, 'rb'), timeout=200)
+                elif ".mp4" in item or ".mov" in item:
+                    bot.send_chat_action(message.chat.id, action='upload_video')
+                    bot.send_video(chat_id=message.chat.id, video=open(item, 'rb'), timeout=200)
+
+    print('Bot is running...')
+    bot.infinity_polling()
+
+if __name__ == "__main__":
+    tbot()
