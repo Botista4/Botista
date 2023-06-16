b9723817effce839b6d4fbda29f321ba76fdf8e2
Botista4 <135262016+Botista4@users.noreply.github.com>
2023-06-16 20:01:50 +0300

    Update main.py fasttttt

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import telebot
 from instagrapi import Client
 from datetime import datetime
-# from server import server
+from server import server
 
 
-API_KEY = '6262533922:AAF4kC8ZJMxfJZdPbVCayTVEpocDyMsu2gE'
+API_KEY = '6262533922:AAF6R3TIjcvBU_oo_wMoMgokFf7d60uLXig'
 CHATID = '5966905118'
 
 bot = telebot.TeleBot(API_KEY)
 
 cl = Client()
-# cl.login('botistareal', 'botist44')
-# server()
+cl.login('botistareal', 'botist44')
+server()
 
 def tbot():
     def chat(message):
@@ -49,6 +49,7 @@ def tbot():
             bot.reply_to(message, smsg)
         if "@" in message.text:
             try:
+                bot.send_chat_action(message.chat.id, 'upload_photo')
                 m = message.text.replace("/pfp @", "")
                 x = cl.user_info_by_username(m)
                 url = x.profile_pic_url_hd
@@ -60,6 +61,7 @@ def tbot():
 
     @bot.message_handler(func=lambda m: True)
     def get_media(message):
+        delete = bot.reply_to(message, 'Processing...')
         m = message.text
         try: 
             x = cl.media_pk_from_url(m)
@@ -68,32 +70,34 @@ def tbot():
             media = minfo.media_type
             likes = "Likes: " + f'{minfo.like_count:,}'
             caption = minfo.caption_text + "\n\n" + likes
-            views = "Plays: " + f'{minfo.play_count:,}'
         except:
             info = None
             media = None
+            bot.delete_message(message.chat.id, delete.message_id, timeout=10)
             bot.reply_to(message, "Unvalid, plesae send a public Instgram link.")
 
         chat(message)
 
         if info == "clips":
-            caption = caption + "\n" + views
             clip_url = cl.media_info(x).video_url
+            bot.delete_message(message.chat.id, delete.message_id, timeout=10)
             bot.send_chat_action(message.chat.id, action='upload_video')
             bot.send_video(chat_id=message.chat.id, video=clip_url, timeout=200, caption=caption, reply_to_message_id=message.message_id)
 
         if info == "igtv":
-            caption = caption + "\n" + views
             tvurl = cl.media_info(x).video_url
+            bot.delete_message(message.chat.id, delete.message_id, timeout=10)
             bot.send_chat_action(message.chat.id, action='upload_video')
             bot.send_video(chat_id=message.chat.id, video=tvurl, timeout=200, caption=caption, reply_to_message_id=message.message_id)
 
         if media == 1:
             thumbnail = minfo.thumbnail_url
+            bot.delete_message(message.chat.id, delete.message_id, timeout=10)
             bot.send_chat_action(message.chat.id, action='upload_photo')
             bot.send_photo(chat_id=message.chat.id, photo=thumbnail, timeout=200, caption=caption, reply_to_message_id=message.message_id)
 
         if media == 8:
+            bot.delete_message(message.chat.id, delete.message_id, timeout=10)
             bot.reply_to(message, text=caption)
             for f in range(len(minfo.resources)):
                 index = minfo.resources[f]
